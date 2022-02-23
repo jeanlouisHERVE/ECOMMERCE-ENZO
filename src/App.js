d5415d9a510466f9f0b8363f0fba9d8de250ec87
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-02-23 21:55:15 +0100

    install react router dom and build of app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes , Route, } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import FloatingCart from './Components/FloatingCart/FloatingCart'
+import Home from './Pages/Home/Home';
+import Contact from './Pages/Contact/Contact';
+import ShoppingCart from './Pages/ShoppingCart/ShoppingCart'
+import Products from './Pages/Products/Products';
+import ProductShowcase from './Pages/ProductShowcase/ProductsShowcase';
+
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Navbar/>
+        <FloatingCart/>
+        <Route path="/" element={<Home/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/produits" element={<Products/>} />
+        <Route path="/produits/:id" element={ProductShowcase} />
+        <Route path="/shoppingcart" element={<ShoppingCart/>} />
+      </Routes>
+    </Router>
   );
 }
 
