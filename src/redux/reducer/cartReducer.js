4237a806ee1cfb8eddac86164c2b4ea635fc0981
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-02-24 19:33:43 +0100

    mise en place de redux 2

---
+++ src/redux/reducer/cartReducer.js
@@ -0,0 +1,16 @@
+const INITIAL_STATE = {
+    cart: []
+}
+
+export default function cartReducer (state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case "ADDITEM": 
+        return {
+
+        }
+        case "UPDATEITEM": 
+        return {
+            
+        }
+    }
+}
